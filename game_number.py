c3534ff45bf38878da4c85f1ae8824bdf87f12e3
Artem Kapusta bARAHNoID <schastlivchik_artem@mail.ru>
2019-11-23 13:39:58 +0300

    угадай число от 1 до 100 ( показывает загаданнное)

--- game_number.py
+++ game_number.py
@@ -5,11 +5,17 @@ number= random.randint(1,100)
 #удалить строку ниже , чтобы не показывало ответ
 print (number)
 
-user_number = int(input('введите число...'))
-if number==user_number :
-    print ('вы угадали, держите пять!')
-elif number != user_number and (user_number<100 and user_number>0):
-    print (' к сожалению вы не угадали....попробуйте еще раз?!...')
-else :
-    print('неправильное число, надо выбирать от 1 до 100...')
+while True:
+    user_number = int(input('введите число...'))
+    if number==user_number :
+        print ('вы угадали, держите пять!')
+        break
+    elif number != user_number and (user_number<100 and user_number>0):
+        print (' к сожалению вы не угадали....попробуйте еще раз?!...')
+        if number < user_number:
+            print('ваше число больше загаданного...')
+        else :
+            print('ваше число меньше загаданного...')
+    else :
+        print('неправильное число, надо выбирать от 1 до 100...')
 
