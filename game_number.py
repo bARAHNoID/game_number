41b2570f1a761a9915a15a54de083e257c4876de
Artem Kapusta bARAHNoID <schastlivchik_artem@mail.ru>
2019-11-23 13:25:11 +0300

    угадай число от 1 до 100 ( показывает загаданнное)

---
+++ game_number.py
@@ -0,0 +1,14 @@
+import random
+
+number= random.randint(1,100)
+#удалить строку ниже , чтобы не показывало ответ
+print (number)
+
+user_number = int(input('введите число...'))
+if number==user_number :
+    print ('вы угадали, держите пять!')
+elif number != user_number and (user_number<100 and user_number>0):
+    print (' к сожалению вы не угадали....попробуйте еще раз?!...')
+else :
+    print('неправильное число, надо выбирать от 1 до 100...')
+
